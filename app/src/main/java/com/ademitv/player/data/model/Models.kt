b5aefdf89fa763e13f899084db94510b10e54fd5
eGoistina <silenproba@gmail.com>
2026-08-02 15:02:45 +0300

    Create Models.kt

---
+++ app/src/main/java/com/ademitv/player/data/model/Models.kt
@@ -0,0 +1,10 @@
+package com.ademitv.player.data.model
+
+data class Channel(
+    val id: String,
+    val name: String,
+    val logoUrl: String,
+    val groupTitle: String,
+    val streamUrl: String
+)
+
